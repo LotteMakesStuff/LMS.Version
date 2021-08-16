dc83b8984576f25e97fb2b425578047f2074ec4a
LotteMakesStuff <lotte@LotteMakesStuff.pink>
2021-08-16 01:00:57 +0100

    Initial commit

---
+++ Assets/Version/Version.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace LMS.Version
+{
+    [CreateAssetMenu(menuName = "Gunsport/Version", fileName = "Version", order = 0)]
+    public class Version : ScriptableObject
+    {
+        static Version instance;
+
+        public VersionNumber GameVersion = new VersionNumber();
+        [SerializeField]private ExtraVersionNumber[] extraVersions;
+
+        public string GitHash;
+        public string BuildTimestamp;
+
+        void OnEnable()
+        {
+            if (instance != null) return;
+            instance = this;
+
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            GameVersion.Initialize();
+            if (extraVersions != null)
+            {
+                foreach (var extraVersion in extraVersions)
+                {
+                    extraVersion.Version.Initialize();
+                }
+            }
+
+            if (!Application.isEditor)
+            {
+                var logOutput = "--------------------------\n"
+                                + $"-- Version: {GameVersion}\n"
+                                + $"-- Commit: {GitHash}\n"
+                                + $"-- Built: {BuildTimestamp}\n"
+                                + "--------------------------";
+                Debug.Log(logOutput);
+            }
+        }
+
+        void OnDisable()
+        {
+            instance = null;
+        }
+
+        public static string GetGameVersion(VersionDeliniator deliniator = VersionDeliniator.Dot)
+        {
+            if (instance == null)
+            {
+                return "no version asset found";
+            }
+            
+            return instance.GameVersion.Version(deliniator);
+        }
+
+        public static string GetExtraVersion(string name, VersionDeliniator deliniator = VersionDeliniator.Dot)
+        {
+            if (instance == null)
+            {
+                return "no version asset found";
+            }
+            
+            var v = GetExtraVersion(name);
+            if (v != null)
+            {
+                return v.Version(deliniator);
+            }
+
+            return name + " is not a valid version";
+        }
+
+        public static VersionNumber GetExtraVersion(string name)
+        {
+            if (instance.extraVersions != null)
+            {
+                foreach (var version in instance.extraVersions)
+                {
+                    if (version.VersionName.ToLower() == name.ToLower()) return version.Version;
+                }
+            }
+
+            return null;
+        }
+
+        public static Version Instance
+        {
+            get
+            {
+                if (!(instance is null)) return instance;
+                return null;
+            }
+        }
+    }
+
+    [Serializable]
+    public class VersionNumber
+    {
+        public int Major;
+        public int Minor;
+        public int Build;
+
+        private Dictionary<VersionDeliniator, string> version;
+
+        public string Version(VersionDeliniator deliniator = VersionDeliniator.Dot) => version[deliniator];
+
+        public void Initialize()
+        {
+            version = new Dictionary<VersionDeliniator, string>()
+            {
+                {VersionDeliniator.Dot, $"{Major}.{Minor}.{Build}"},
+                {VersionDeliniator.Underscore, $"{Major}_{Minor}_{Build}"}
+            };
+        }
+
+        public override string ToString()
+        {
+            return version[VersionDeliniator.Dot];
+        }
+    }
+
+    [Serializable]
+    public class ExtraVersionNumber
+    {
+        public string VersionName;
+        public VersionNumber Version;
+    }
+
+    public enum VersionDeliniator
+    {
+        Dot,
+        Underscore
+    }
+}
---
+++ Assets/Version/VersionDisplayBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LMS.Version
+{
+    public abstract class VersionDisplayBase : MonoBehaviour
+    {
+        [Multiline]
+        public string VersionFormatString = "game v{v}\n{gitHash}";
+        
+        public string GetFormattedString(string format)
+        {
+            if (Version.Instance == null)
+            {
+                return "No Version asset found\n" + format;
+            }
+            
+            var outString = format.Replace("{v}", Version.GetGameVersion(VersionDeliniator.Dot));
+            outString = outString.Replace("{v_}", Version.GetGameVersion(VersionDeliniator.Underscore));
+            outString = outString.Replace("{gitHash}", Version.Instance.GitHash);
+            outString = outString.Replace("{time}", Version.Instance.BuildTimestamp);
+
+            // handle extra versions in the format of {v:versionName}
+            bool hasExtraVersions = true;
+            while (hasExtraVersions)
+            {
+                var start = outString.IndexOf("{v:", StringComparison.InvariantCulture);
+                if (start != -1)
+                {
+                    var end = outString.IndexOf("}", StringComparison.InvariantCulture);
+
+                    var versionKey = outString.Substring(start + 3, end - start - 3);
+                    outString = outString.Replace(outString.Substring(start, end - start + 1),
+                        Version.GetExtraVersion(versionKey, VersionDeliniator.Dot));
+                }
+                else
+                {
+                    hasExtraVersions = false;
+                }
+            }
+
+            return outString;
+        }
+    }
+
+}
